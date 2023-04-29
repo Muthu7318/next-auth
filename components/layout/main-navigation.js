ba9b66c5e1aeed8235bfcd9272bfea85584cd845
muthukumar <muthumarshal@gmail.com>
2023-04-29 19:01:07 +0530

    signout

--- components/layout/main-navigation.js
+++ components/layout/main-navigation.js
@@ -1,11 +1,15 @@
 import Link from "next/link";
-import { useSession } from "next-auth/react";
+import { useSession, signOut } from "next-auth/react";
 
 import classes from "./main-navigation.module.css";
 
 function MainNavigation() {
   const { data: session } = useSession();
   console.log(session);
+
+  function logoutHandler() {
+    signOut();
+  }
 
   return (
     <header className={classes.header}>
@@ -28,7 +32,7 @@ function MainNavigation() {
           )}
           {session && (
             <li>
-              <button>Logout</button>
+              <button onClick={logoutHandler}>Logout</button>
             </li>
           )}
         </ul>
