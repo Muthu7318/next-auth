15eebc375571704e0f53491f06d307ae3e5f62ea
muthukumar <muthumarshal@gmail.com>
2023-04-29 19:21:42 +0530

    client side protection for pages

--- components/profile/user-profile.js
+++ components/profile/user-profile.js
@@ -1,15 +1,28 @@
-import ProfileForm from './profile-form';
-import classes from './user-profile.module.css';
+import ProfileForm from "./profile-form";
+import classes from "./user-profile.module.css";
+import { useSession } from "next-auth/react";
 
 function UserProfile() {
   // Redirect away if NOT auth
+  const { data: session, status } = useSession();
 
-  return (
-    <section className={classes.profile}>
-      <h1>Your User Profile</h1>
-      <ProfileForm />
-    </section>
-  );
+  if (status === "loading") {
+    return <p className={classes.profile}>Loading...</p>;
+  }
+
+  if (!session) {
+    window.location.href = "/auth";
+    return;
+  }
+
+  if (status === "authenticated") {
+    return (
+      <section className={classes.profile}>
+        <h1>Your User Profile</h1>
+        <ProfileForm />
+      </section>
+    );
+  }
 }
 
 export default UserProfile;
